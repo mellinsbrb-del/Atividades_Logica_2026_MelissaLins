891248264d02a2974a12a2f6eb4a8a2c60deeadb
mellinsbrb-del <mellinsbrb@gmail.com>
2026-05-14 16:55:54 -0300

    Rename Calculadora de IMC.cs to Calculadora de IMC Melissa.cs

---
+++ Calculadora de IMC Melissa.cs
@@ -0,0 +1,21 @@
+﻿
+Console.WriteLine(" Calculadora de IMC ");
+Console.Write("Digite seu peso: ");
+string inputPeso = Console.ReadLine().Replace(",", "."); 
+float.TryParse(inputPeso, out float peso);
+
+Console.Write("Digite sua altura (ex: 1.75): ");
+string inputAltura = Console.ReadLine().Replace(",", "."); 
+float.TryParse(inputAltura, out float altura);
+
+if (altura > 0)
+{
+    float imc = peso / (altura * altura);
+
+    Console.WriteLine($ "Seu IMC é: {imc:F2}");
+
+    else if (imc < 18.5) Console.WriteLine("Classificação: Abaixo do peso");
+    else if (imc < 25.0) Console.WriteLine("Classificação: Peso normal");
+    else if (imc < 29.9) Console.WriteLine("Classificação: Acima do peso");
+    else if (imc > 40.0) Console.WriteLine("Classificação: Obesidade");
+}
